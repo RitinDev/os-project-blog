82b3ea195e671103eaa4d53869e359e5531627fc
Ritin Malhotra <rm5486@nyu.edu>
2023-02-28 11:20:47 +0400

    Add Global Styles

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,7 +1,6 @@
 import styles from '../../styles/Home.module.css';
 import Link from 'next/link'
 import Head from 'next/head'
-import Layout from '../components/layout';
 
 export default function FirstPost() {
     return (
@@ -9,10 +8,8 @@ export default function FirstPost() {
             <Head>
                 <title>First Post</title>
             </Head>
-            <Layout>
                 <h1 className={styles.title}>First Post</h1>
                 <h2 className={styles.description}>Back to <Link href={"/"}>home!</Link></h2>
-            </Layout>
         </>
     )
 }
